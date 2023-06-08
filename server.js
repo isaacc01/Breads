ba4b3311e8bd16a858500477c82a3aa96e0ee79d
Isaac Hernandez <Isaac@Isaacs-Air.attlocal.net>
2023-06-07 19:32:13 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -7,17 +7,24 @@ require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 
+// ROUTES
+app.get('/', (req, res) =>{
+  res.send('Welcome to an Awesome App about Breads')
+})
+
 //middleware
 const breadController = require('./controllers/breads_controller.js')
 app.use('/breads', breadController)
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-// ROUTES
-app.get('/', (req, res) =>{
-    res.send('Welcome to an Awesome App about Breads')
-  })
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('Not Found')
+})
+
 
 app.listen(PORT, ()=>(
     console.log('listening on port:', PORT)
