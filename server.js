709cef2ca637c79dbd838df67b78055d64549688
Isaac Hernandez <Isaac@Isaacs-Air.attlocal.net>
2023-06-24 10:37:43 -0700

    final commit for breads

--- server.js
+++ server.js
@@ -1,49 +1,37 @@
-// DEPENDENCIES
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
-
 // CONFIGURATION
 require('dotenv').config()
-const PORT = process.env.PORT
-console.log(PORT)
 
-// ROUTES
-app.get('/', (req,res)=>{
-    es.send('Welcome to my Bread World!')
+app.get('/', (req,res) =>{
+    res.send('Welcome to an Awesome Bread App')
 })
 
-mongoose.connect(process.env.MONGO_URI, 
-  {useNewUrlParser: true, useUnifiedTopology: true},
-  ()=>{console.log('connected to mongoDB: ', process.env.MONGO_URI)})
 
-
-//middleware
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json());
+// MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
-
-//BREAD
-const breadController = require('./controllers/breads_controller.js')
-app.use('/breads', breadController)
+const breadsController = require('./controllers/breads_controller')
+app.use('/breads', breadsController)
 
 //BAKERS
-const bakersController = require('./controllers/bakers_controller.js')
+const bakersController = require('./controllers/bakers_controller')
 app.use('/bakers', bakersController)
 
+const PORT = process.env.PORT || 5000
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {console.log('connected to : ', process.env.MONGO_URI)}
+    )
 // 404 Page
 app.get('*', (req, res) => {
-  res.send('Not Found')
-})
+    res.send('404')
+  })
 
-
-app.listen(PORT, ()=> {
-  console.log('listening on port: ', PORT)
-})
-
+app.listen(process.env.PORT)
